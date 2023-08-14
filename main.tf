586e8495621a5d617397f9eb889b33958dde1367
SemProject2024 <semproject2024@gmail.com>
2023-08-14 12:57:13 +0530

    Further Modification
    Signed-off-by: SemProject2024 <semproject2024@gmail.com>

--- main.tf
+++ main.tf
@@ -15,7 +15,8 @@ client_id = ""
 client_secret=""
 }
 
-#Creating Resource Group name - test-rg-2
+#Creating Resource Group name - test-rg-2 this is modified by me
+
 resource "azurerm_resource_group" "rg" {
     name="test-rg-2"
     location = "EastUS"
