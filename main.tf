5ea162dbc2f7546deaad408b8edce2764830aab9
SemProject2024 <semproject2024@gmail.com>
2023-08-14 12:38:37 +0530

    Removed Credentials
    Signed-off-by: SemProject2024 <semproject2024@gmail.com>

--- main.tf
+++ main.tf
@@ -9,10 +9,10 @@ terraform {
 provider "azurerm"{
 features {}
 skip_provider_registration = "true"
-subscription_id = "72a851c4-6ce9-4328-902a-1b4f3e431554"
-tenant_id = "f2a009da-b491-4dbb-94e8-5809162549cd"
-client_id = "9e46e6ee-df6f-438d-bf40-6dbfa737777c"
-client_secret="szF8Q~xjzDW2_IFD0yyaOZE_SjkvX8TysLW86c8T"
+subscription_id = ""
+tenant_id = ""
+client_id = ""
+client_secret=""
 }
 
 #Creating Resource Group
