446e6d58bc98707bbee04fbe899c3c1157c4d607
SemProject2024 <semproject2024@gmail.com>
2023-08-14 11:09:13 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">=2.7"
+    }
+  }
+}
+provider "azurerm"{
+features {}
+skip_provider_registration = "true"
+subscription_id = "72a851c4-6ce9-4328-902a-1b4f3e431554"
+tenant_id = "f2a009da-b491-4dbb-94e8-5809162549cd"
+client_id = "9e46e6ee-df6f-438d-bf40-6dbfa737777c"
+client_secret="szF8Q~xjzDW2_IFD0yyaOZE_SjkvX8TysLW86c8T"
+}
+
+resource "azurerm_resource_group" "rg" {
+    name="test-rg-1"
+    location = "EastUS"
+  
+}
