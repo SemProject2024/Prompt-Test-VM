984675fce6b70ae4d3e0dd9f776fcd52d81cc548
SemProject2024 <semproject2024@gmail.com>
2023-09-11 10:29:33 +0530

    Modified destroy added file

--- ssh_key_based.py
+++ ssh_key_based.py
@@ -17,23 +17,16 @@ def run(template):
         ssh_client.connect(ip_address, username=username, pkey=private_key)
         
         print("Connected to VM via SSH")
-        
-        #DESTROYING RESOURCES
-        stdin, stdout, stderr = ssh_client.exec_command("terraform destroy -auto-approve")
-        print("Executing terraform destroy")
+        command = f'echo "{file_content}" > main.tf'
+        stdin, stdout, stderr = ssh_client.exec_command("echo '%s' > main.tf "%file_content)
+        print("Updated main.tf")
+        stdin, stdout, stderr = ssh_client.exec_command("terraform init")
+        output = stdout.read().decode("utf-8")
+        print("Executed terraform init")
+        stdin, stdout, stderr = ssh_client.exec_command("terraform plan -lock=false")
+        print("Executing terraform plan")
         output = stdout.read().decode("utf-8")
         print(output)
-        
-        
-        # command = f'echo "{file_content}" > main.tf'
-        # stdin, stdout, stderr = ssh_client.exec_command("echo '%s' > main.tf "%file_content)
-        # print("Updated main.tf")
-        # stdin, stdout, stderr = ssh_client.exec_command("terraform init")
-        # output = stdout.read().decode("utf-8")
-        # print("Executed terraform init")
-        # stdin, stdout, stderr = ssh_client.exec_command("terraform plan -lock=false")
-        # print("Executing terraform plan")
-        # output = stdout.read().decode("utf-8")
         # stdin, stdout, stderr = ssh_client.exec_command("terraform apply -auto-approve")
         # print("Executing terraform apply")
         # output = stdout.read().decode("utf-8")
@@ -45,7 +38,14 @@ def run(template):
         
         # # Execute commands or perform operations on the VM if needed
         
-        # ssh_client.close()
-        # print("SSH connection closed")
+        
+        # #DESTROYING RESOURCES
+        # stdin, stdout, stderr = ssh_client.exec_command("terraform destroy -auto-approve")
+        # print("Executing terraform destroy")
+        # output = stdout.read().decode("utf-8")
+        # print(output)
+        
+        ssh_client.close()
+        print("SSH connection closed")
     except Exception as e:
         print(f"An error occurred: {str(e)}")
