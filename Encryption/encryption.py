e5fc60edd48af2a180273838a5f212779e04f60d
SemProject2024 <142200140+SemProject2024@users.noreply.github.com>
2023-10-31 15:31:05 +0530

    Update encryption.py

--- Encryption/encryption.py
+++ Encryption/encryption.py
@@ -1,61 +1,41 @@
-import hashlib
-client='client-secret'
-result = hashlib.sha256(client.encode()) 
-print("Result: ",result.hexdigest())
-final_hash=result.hexdigest()
 
 from cryptography.fernet import Fernet
-import cryptography.fernet
-import base64
-import os
-from cryptography.fernet import Fernet
-from cryptography.hazmat.primitives import hashes
-from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 
-message = "<your-message-will-be-this~~./askperntqwe>"
-password = b"client-secret"
-salt = bytes(final_hash.encode())
-print("salt",salt)
-kdf = PBKDF2HMAC(
-    algorithm=hashes.SHA256(),
-    length=32,
-    salt=salt,
-    iterations=150,
-)
-key = base64.urlsafe_b64encode(kdf.derive(password))
+def encrypt(key,input):
+    key = keygen(key)
+    message = input
+    fernet = Fernet(key)
+    print("key is ",key)
+    encMessage = fernet.encrypt(message.encode())
+    print("original string: ", message)
+    print("encrypted string: ", encMessage)
+    return encMessage
 
-fernet = Fernet(key)
-print("key is ",key)
-encMessage = fernet.encrypt(message.encode())
+def decrypt(key,input):
+    key = keygen(key)
+    fernet = Fernet(key)
+    decMessage = fernet.decrypt(input).decode()
+    print("decrypted string: ", decMessage)
+    return decMessage
 
-print("original string: ", message)
-print("encrypted string: ", encMessage)
+def keygen(key):
+    import hashlib
+    client=key
+    result = hashlib.sha256(client.encode()) 
+    print("Result: ",result.hexdigest())
+    final_hash=result.hexdigest()
+    import base64
+    from cryptography.hazmat.primitives import hashes
+    from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
+    password = b"client-secret"
+    salt = bytes(final_hash.encode())
+    print("salt",salt)
+    kdf = PBKDF2HMAC(
+        algorithm=hashes.SHA256(),
+        length=32,
+        salt=salt,
+        iterations=200,
+    )
+    key = base64.urlsafe_b64encode(kdf.derive(password))
+    return key
 
-decMessage = fernet.decrypt(encMessage).decode()
-print("decrypted string: ", decMessage)
-
-
-
-"""import hashlib
-client='client-secret'
-result = hashlib.sha256(client.encode()) 
-print("Result: ",result.hexdigest())
-
-from cryptography.fernet import Fernet
-import cryptography.fernet
-import base64
-import os
-from cryptography.fernet import Fernet
-from cryptography.hazmat.primitives import hashes
-from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
-
-message = "<your-message-will-be-this~~./askperntqwe>"
-key = base64.urlsafe_b64encode(f"b'{result.hexdigest()}'")
-fernet = Fernet(key)
-print("key is ",key)
-encMessage = fernet.encrypt(message.encode())
-print("original string: ", message)
-print("encrypted string: ", encMessage)
-decMessage = fernet.decrypt(encMessage).decode()
-print("decrypted string: ", decMessage)
-"""
